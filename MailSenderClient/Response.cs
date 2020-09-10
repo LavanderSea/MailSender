743cb7f13df0e4475f14090becda8b2e327c46dc
Татьяна Кузнецова <tanya.smith1712@gmail.com>
2020-09-10 15:09:19 +0300

    rename a project

--- MailSenderClient/Response.cs
+++ MailSenderClient/Response.cs
@@ -1,18 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace MailSenderClient
+﻿namespace MailSenderClient
 {
     public class Response
     {
-        public string Result { get;}
-        public string FailedMessage { get;}
-
         public Response(string result, string failedMessage)
         {
             Result = result;
             FailedMessage = failedMessage;
         }
+
+        public string Result { get; }
+        public string FailedMessage { get; }
     }
 }
